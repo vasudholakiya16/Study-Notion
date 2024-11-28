642306b1d5df7bd9d8331f1fad0b10b6a8e6c8bc
vasudholakiya16 <122475789+vasudholakiya16@users.noreply.github.com>
2024-11-28 08:44:02 +0530

    add navigation and footer links; update App structure and dependencies

--- src/index.js
+++ src/index.js
@@ -1,28 +1,20 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import "./index.css";
-import { BrowserRouter } from "react-router-dom";
-import { Provider } from "react-redux";
-import rootReducer from "./reducer";
-import {configureStore} from "@reduxjs/toolkit"
-import { Toaster } from "react-hot-toast";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom'; // Import the BrowserRouter component from the react-router-dom library
+import App from './App'; // Import the App component
+import './index.css'; // Optional, you can add custom styles here.
 
-
-const store = configureStore({
-  reducer:rootReducer,
-});
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
+// Renders the App component into the root div in the HTML file
+ReactDOM.render(
   <React.StrictMode>
-  <Provider store = {store}>
     <BrowserRouter>
-        <App />
-        <Toaster/>
-      </BrowserRouter>
-  </Provider>
     
+    <App />
+    </BrowserRouter>
     
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
+
+// Optional: Track performance metrics
+// reportWebVitals();
